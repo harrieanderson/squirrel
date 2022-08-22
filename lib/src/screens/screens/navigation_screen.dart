aea15bbb891be4cfc37fc7c465cfcce1c02fa97f
harrieanderson <68598031+harrieanderson@users.noreply.github.com>
2022-08-22 19:17:02 +0100

    Add files via upload

---
+++ lib/src/screens/screens/navigation_screen.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:squirrel/src/screens/add_post_screen.dart';
+import 'package:squirrel/src/screens/googlemaps/google_map_screen.dart';
+import 'package:squirrel/src/screens/home_screen.dart';
+import 'package:squirrel/src/screens/messages/message.dart';
+import 'package:squirrel/src/screens/profile_page.dart';
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  var current_index = 0;
+  final screens = [
+    HomeScreen(
+      uid: FirebaseAuth.instance.currentUser!.uid,
+    ),
+    GoogleMapScreen(
+      key: UniqueKey(),
+      uid: FirebaseAuth.instance.currentUser!.uid,
+    ),
+    MessagesScreen(),
+    ProfilePageUi(
+      key: UniqueKey(),
+      uid: FirebaseAuth.instance.currentUser!.uid,
+      visitedUserId: FirebaseAuth.instance.currentUser!.uid,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: current_index,
+        children: screens,
+      ),
+      floatingActionButton: current_index == 0
+          ? FloatingActionButton(
+              heroTag: null,
+              elevation: 3,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddPostScreen(
+                      uid: FirebaseAuth.instance.currentUser!.uid,
+                    ),
+                  ),
+                );
+              },
+              child: Icon(Icons.add),
+            )
+          : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: IconButton(
+                    onPressed: () => _onItemTapped(0),
+                    icon: Icon(Icons.home),
+                    color: current_index == 0 ? Colors.red : null),
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: () {
+                    _onItemTapped(1);
+                  },
+                  icon: Icon(Icons.fmd_good),
+                  color: current_index == 1 ? Colors.red : null,
+                ),
+              ),
+              AnimatedContainer(
+                duration: Duration(milliseconds: 200),
+                height: 2,
+                width: current_index == 0 ? 60 : 0,
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: () {
+                    _onItemTapped(2);
+                  },
+                  icon: Icon(Icons.message),
+                  color: current_index == 2 ? Colors.red : null,
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  onPressed: () {
+                    _onItemTapped(3);
+                  },
+                  icon: Icon(Icons.person),
+                  color: current_index == 3 ? Colors.red : null,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      current_index = index;
+    });
+  }
+}
---
+++ lib/src/screens/screens/profile_page.dart
@@ -0,0 +1,244 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'dart:typed_data';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:squirrel/models/post.dart';
+import 'package:squirrel/models/repo.dart';
+import 'package:squirrel/models/usser_model.dart';
+import 'package:squirrel/services/database.dart';
+import 'package:squirrel/src/widgets/PostContainer.dart';
+import 'package:squirrel/utils/utils.dart';
+
+const _kAvatarRadius = 45.0;
+const _kAvatarPadding = 8.0;
+
+class ProfilePageUi extends StatefulWidget {
+  final String uid;
+  final String visitedUserId;
+
+  const ProfilePageUi(
+      {Key? key, required this.visitedUserId, required this.uid})
+      : super(key: key);
+
+  @override
+  _ProfilePageUiState createState() => _ProfilePageUiState();
+}
+
+class _ProfilePageUiState extends State<ProfilePageUi> {
+  late var _isLoggedOnUser =
+      userModel!.uid == FirebaseAuth.instance.currentUser;
+  Uint8List? _image;
+
+  UserModel? userModel;
+
+  List<Post> _allPosts = [];
+
+  Widget showProfilePosts(UserModel author) {
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: AlwaysScrollableScrollPhysics(),
+        itemCount: _allPosts.length,
+        itemBuilder: (context, index) {
+          return PostContainer(
+            post: _allPosts[index],
+            author: author,
+            currentUserId: widget.uid,
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
+  getAllPosts() async {
+    List<Post> userPosts =
+        await DatabaseMethods.getUserPosts(widget.visitedUserId);
+    if (mounted) {
+      setState(() {
+        _allPosts = userPosts;
+      });
+    }
+  }
+
+  getData() async {
+    try {
+      userModel = await Repo.getUser(widget.uid);
+      setState(() {});
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  void initState() {
+    super.initState();
+    getAllPosts();
+    getData();
+    getAllPosts();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.notifications),
+            color: Colors.black,
+            iconSize: 35,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(_kAvatarPadding),
+                child: FutureBuilder<UserModel>(
+                  future: Repo.getUser(widget.uid),
+                  builder: (context, snapshot) {
+                    final userModel = snapshot.data;
+                    if (userModel == null) {
+                      return Container();
+                    }
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              userModel.photoUrl,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              Column(
+                children: [
+                  Text(
+                    userModel!.username,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(userModel!.bio)
+                ],
+              ),
+            ],
+          ),
+          if (_isLoggedOnUser != true)
+            Container()
+          else
+            Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                ),
+                TextButton.icon(
+                  style: TextButton.styleFrom(
+                    textStyle: TextStyle(color: Colors.green, fontSize: 15),
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+                  onPressed: () => {},
+                  icon: Icon(
+                    Icons.person_add,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Add friend',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                TextButton.icon(
+                  style: TextButton.styleFrom(
+                    textStyle: TextStyle(color: Colors.blue, fontSize: 15),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24.0),
+                    ),
+                  ),
+                  onPressed: () => {},
+                  icon: Icon(
+                    Icons.mail,
+                  ),
+                  label: Text(
+                    'Message',
+                  ),
+                ),
+              ],
+            ),
+          Divider(
+            thickness: 1,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 131,
+              ),
+              Column(
+                children: [
+                  Text(
+                    userModel!.culls.toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    'Culls',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Row(
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        userModel!.culls.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Text(
+                        'Friends',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Divider(
+            thickness: 1,
+          ),
+          showProfilePosts(
+            userModel!,
+          ),
+        ],
+      ),
+    );
+  }
+}
